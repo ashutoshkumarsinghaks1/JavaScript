e8d9dba60f659ca61577082c729b87266ed9386e
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2024-05-27 21:27:32 +0530

    Add files via upload

---
+++ 003-Javascript ARITHMETIC OPERATORS/index.js
@@ -0,0 +1,56 @@
+// arithematic operators = operands(values , variables , etc)
+//                         operators (+ - * /)
+//                         ex. 11 = x + 5
+let students = 30;
+
+// students = students + 1;
+// students = students -1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students  %2;
+
+// students +=1;
+// students -=1;
+// students *=2;
+// students /=2;
+// students **= 2;
+
+// increment operator => to increment by only one (1)
+
+// students++;
+// console.log(students)
+
+// decreemnt operator
+// students--;
+// console.log(students)
+
+/*
+operator precedence
+1.parenthesis ()
+2.exponents
+3.multiplication & division & modulo
+4.addition & subtraction
+ */
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+// console.log(result); // ouput: 23
+
+// 1 +  2 * 3 + 16
+// 1 + 6 + 16
+// 23
+
+//let result1 = (12 % 5) + 8 / 2;
+//console.log(result1); // output : 6
+
+// (12 % 5) + 8 / 2
+// 2 + 4
+//6
+
+let result2 = 6 / 2 ** (2 + 5);
+console.log(result2);
+
+// 6 / 2 ** (2 + 5)
+// 6  / 2 ** 7
+// 6 / 128
+// 0.046875
